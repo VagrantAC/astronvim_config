882ecbcee6afc4708dfda3fbeda6f03942be2e7a
VagrantAC <vagrantac@126.com>
2023-06-24 21:33:46 +0800

    add coc

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,7 +3,14 @@ return {
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
+  {
+    "neoclide/coc.nvim",
+    branch = "release",
+    lazy = true,
+    init = function(self)
+    end,
+    cmd = {"CocInstall", "CocCommand"}
+  }
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
